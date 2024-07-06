429600ad916ffe597d6e02d41a174a973a737e6c
maindenis <165322876+maindenis@users.noreply.github.com>
2024-07-06 16:44:03 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,4 @@
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
-function getModalsParams() {
-    $(".modal_sect").css({
-        "height" : "auto"
-    });
-    $(".modal_sect").css({
-        "height" : $(window).height() - $("#header").height() - $("#footer").height() + "px"
-    });
-}
-
-$(window).resize(function() {
-// getModalsParams();
-});
-
-$(document).scroll(function() {
-// getModalsParams();
-});
-
 $(document).ready(function() {
-// getModalsParams();
     const swiper = new Swiper('.slider', {
         loop: true,
         navigation: {
